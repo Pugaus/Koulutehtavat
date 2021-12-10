c8a2147e7fec5d26382e270050addc1c6d9b8dd8
Teemu <puukka92@gmail.com>
2021-12-10 13:06:40 +0200

    Tehtava 1 ja 2 valmis

---
+++ Ohjelmoinnin perusteet ja Version hallinta/01_Tulostaminen/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01_Tulostaminen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tee sovellus, joka tulostaa konsolin ruudulle tekstin "Hei maailma!".
+            //Muuta sovellusta, että se kysyy käyttäjän nimen ja iän sekä tervehtii sitten häntä esimerkiksi tähän tapaan "Hei Maija, 26!"
+
+            int ika;
+            string nimi;
+                        
+            Console.WriteLine("Hei maailma!");
+            Console.WriteLine("Kerro nimesi:");
+            nimi = Console.ReadLine();
+
+            Console.WriteLine("Kerro myös ikäsi:");
+            ika = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hei " + nimi + ", " + ika + "!");
+        }
+    }
+}
---
+++ Ohjelmoinnin perusteet ja Version hallinta/02_Laskeminen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02_Laskeminen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku = 0;
+
+            Console.WriteLine("Anna jokin luku 1 - 10 väliltä:");
+            luku = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("1*" + luku + "=" + 1 * luku + " 1/" + luku + "=" + 1 / luku + " 1%" + luku + "=" + 1 % luku);
+            Console.WriteLine("2*" + luku + "=" + 2 * luku + " 2/" + luku + "=" + 2 / luku + " 1%" + luku + "=" + 2 % luku);
+            Console.WriteLine("3*" + luku + "=" + 3 * luku + " 3/" + luku + "=" + 3 / luku + " 1%" + luku + "=" + 3 % luku);
+            Console.WriteLine("4*" + luku + "=" + 4 * luku + " 4/" + luku + "=" + 4 / luku + " 1%" + luku + "=" + 4 % luku);
+            Console.WriteLine("5*" + luku + "=" + 5 * luku + " 5/" + luku + "=" + 5 / luku + " 1%" + luku + "=" + 5 % luku);
+        }
+    }
+}
