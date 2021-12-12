bf67133bf0647fea044b6395eb14eaaf4a666ea9
Teemu <puukka92@gmail.com>
2021-12-12 12:03:33 +0200

    Tehtävä 5 ja 6 valmis.

---
+++ Ohjelmoinnin perusteet ja Version hallinta/05_Ehtolause/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _05_Ehtolause
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vaihe 1.Tee ohjelma joka kysyy käyttäjän iän.Jos käyttäjä on alle 13 vuotta, ohjelma tervehtii "Mitä poitsu".
+            //Jos käyttäjä on 14 - 25, ohjelma tervehtii "Mitä nuorimies".Jos käyttäjä on 26 - 56, ohjelma tervehtii "Olet mies parhaassa iässä".
+
+            //Vaihe 2.Lisää ohjelmaan toiminne, joka kysyy aluksi onko käyttäjä mies vai nainen.Naiselle lisää samoihin ikäjakaumiin sopivat tervehdykset.
+
+            //Vaihe 3.Lisää vielä tervehdys kaikille yli 56 vuotiaille
+            
+            int numero, sukupuoli;
+            
+            Console.WriteLine("Paina 1 jos olet mies. Paina 2 jos olet nainen: ");
+            while (!int.TryParse(Console.ReadLine(), out sukupuoli) || sukupuoli > 2)
+            {
+                Console.Write("Paina 1 jos olet mies. Paina 2 jos olet nainen: ");
+            }
+            
+            Console.WriteLine("Kerro ikäsi");          
+            while (!int.TryParse(Console.ReadLine(), out numero))
+            {
+                Console.Write("Syötä vain numeroita: ");
+            }
+
+            
+            switch (sukupuoli)
+            {
+                case 1:
+                    if (numero <= 12)
+                    {
+                        Console.WriteLine("Mitä poitsu");
+                    }
+                    else if (numero == 14 || numero <= 25)
+                    {
+                        Console.WriteLine("Mitä nuorimies");
+                    }
+                    else if (numero == 26 || numero <= 56)
+                    {
+                        Console.WriteLine("Olet mies parhaassa iässä");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hei ukki!");
+                    }
+                    break;
+                case 2:
+                    if (numero <= 12)
+                    {
+                        Console.WriteLine("Mitä tytsy");
+                    }
+                    else if (numero == 14 || numero <= 25)
+                    {
+                        Console.WriteLine("Mitä nuorinainen");
+                    }
+                    else if (numero == 26 || numero <= 56)
+                    {
+                        Console.WriteLine("Olet nainen parhaassa iässä");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hei mummo");
+                    }
+                    break;
+            }         
+        }
+    }
+}
---
+++ Ohjelmoinnin perusteet ja Version hallinta/06_Loogiset_operaattorit/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _06_Loogiset_operaattorit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Paistaako aurinko? \nKYLLÄ = 1 / EI = 2");
+            int sun = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nSataako? \nKYLLÄ = 1 / EI = 2");
+            int rain = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nOnko lämpötila >0 \nKYLLÄ = 1 / EI = 2");
+            int temp = Convert.ToInt32(Console.ReadLine());
+
+            if ((sun == 1) && (rain == 1) && (temp == 1))
+            {
+                Console.WriteLine("\nTaitaa tulla kesä.");
+            }
+
+            else if ((sun == 2) && (rain == 1) && (temp == 1))
+            {
+                Console.WriteLine("\nSyksy saapui lehdet vei tuuli menneessään.");
+            }
+
+            else if ((sun == 2 || sun == 1) && (rain == 2) && (temp == 1))
+                Console.WriteLine("\nJo joutui armas aika ja suvi suloinen.");
+
+
+            else if ((sun == 2) && (rain == 2 || rain == 1) && (temp == 2))
+                Console.WriteLine("\nKylmää ja pimeää");
+
+
+            else if ((sun == 1) && (rain == 1) && (temp == 2))
+                Console.WriteLine("\nKesällä sataa aina");
+
+            else if ((sun == 1) && (rain == 2) && (temp == 1))
+                Console.WriteLine("\nPakkanen paukkuu");
+        }
+    }
+}
