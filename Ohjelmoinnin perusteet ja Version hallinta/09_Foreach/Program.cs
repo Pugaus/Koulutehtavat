5d67709a7368d805a37e25f41afe00b4327eb426
Teemu <puukka92@gmail.com>
2021-12-13 12:18:36 +0200

    Tehtävä 9 valmis

---
+++ Ohjelmoinnin perusteet ja Version hallinta/09_Foreach/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace _09_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, summa = 0;
+            int[] taulukko = new int[5];
+
+            Console.WriteLine("Anna viisi lukua: ");
+            for (int i = 0; i < 5; i++)
+            {
+                while (!int.TryParse(Console.ReadLine(),out numero))
+                {
+                    Console.WriteLine("Syötä vain numeroita: ");
+                }
+                taulukko[i] = numero;
+            }
+
+
+            foreach (var item in taulukko)
+            {
+                summa += item;
+            }
+
+            Console.WriteLine("Keskiarvo on: " + summa / 5);
+        }
+    }
+}
