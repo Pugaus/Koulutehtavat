b29daf28f51dd129fd99c4b426af395bfc27f162
Teemu <puukka92@gmail.com>
2021-12-18 14:31:34 +0200

    Tehtävä 12 valmis

---
+++ Ohjelmoinnin perusteet ja Version hallinta/12_aliohjelma/aliohjelma vaihe1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aliohjelma_vaihe1
+{
+    class Program
+    {
+        private static int Tervehdys(string kayttaja, string suku)
+        {
+            Console.WriteLine("Tervetuloa ohjelmaan!" + " " + kayttaja + " " + suku);
+            Console.Write("Kerro ikäsi: ");
+
+            int ika = int.Parse(Console.ReadLine());
+            return ika;
+        }
+
+        static void Main(string[] args)
+        {
+            int ika;
+
+            Console.WriteLine("Anna nimesi:");
+            string kayttaja = Console.ReadLine();
+            Console.WriteLine("Anna sukunimesi:");
+            string suku = Console.ReadLine();
+            
+            Console.WriteLine();
+            ika = Tervehdys(kayttaja, suku);
+        }
+    }
+}
