106fb1f1917c6b20021638a3cd59e9ecc2e97c8f
Teemu <puukka92@gmail.com>
2021-12-18 14:29:13 +0200

    Tehtävä 10 valmis

---
+++ Ohjelmoinnin perusteet ja Version hallinta/10_While/While vaihe 1/While vaihe 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Vaihe_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int sum = 0;
+
+            while (sum <= 99)
+            {
+                Console.WriteLine("Anna jokin luku 1 - 99 välillä: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                sum += num;
+                Console.WriteLine("\nLaskettu summa on: " + sum);
+            }
+            
+            Console.WriteLine("Raja saavutettu");
+        }
+    }
+}
---
+++ Ohjelmoinnin perusteet ja Version hallinta/10_While/While vaihe 2/While vaihe 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Tehtävä_7._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomnumber = random.Next(1, 46);
+            int quess;
+            bool gameover = false;
+
+            Console.WriteLine("Arvaa luku 1 - 45 välillä. Jos haluat lopettaa ohjelman kirjoita 9000");
+
+            while (gameover == false)
+            {
+                quess = Convert.ToInt32(Console.ReadLine());
+
+                if (quess != randomnumber && quess == 9000)
+                {
+                    Console.WriteLine("Lopetetaan ohjelma.");
+                    gameover = true;
+                }
+                else if (quess == randomnumber)
+                {
+                    Console.WriteLine("Onneksi olkoon, sama luku!");
+                    gameover = true;
+                }
+                else if (quess > randomnumber)
+                {
+                    Console.WriteLine("Numero on pienempi.");
+                }
+                else if (quess < randomnumber)
+                {
+                    Console.WriteLine("Numero on suurempi.");
+                }
+            }
+        }
+    }
+}
