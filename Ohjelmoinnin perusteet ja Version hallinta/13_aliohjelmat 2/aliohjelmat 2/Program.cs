c35ea108a36710654194c7ab7fc36a6389dcda67
Teemu <puukka92@gmail.com>
2021-12-27 20:41:17 +0200

    Tehtävät valmiit

---
+++ Ohjelmoinnin perusteet ja Version hallinta/13_aliohjelmat 2/aliohjelmat 2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funktiot
+{
+    class Program
+    {
+        private static int Pienin(int eka, int toka, int kolmas)
+        {          
+            Console.WriteLine("\nPienin luku oli: " + Math.Min(Math.Min(eka, toka), kolmas));
+            int pieninLuku = Math.Min(Math.Min(eka, toka), kolmas);
+            return pieninLuku;
+        }
+
+        private static int Suurin(int eka, int toka, int kolmas)
+        {
+            Console.WriteLine("Suurin luku oli: " + Math.Max(Math.Max(eka, toka), kolmas));
+            int suurinLuku = Math.Max(Math.Max(eka, toka), kolmas);
+            return suurinLuku;
+        }
+
+        static void Main(string[] args)
+        {
+            int luku1, luku2, luku3, suurinLuku, pieninLuku;
+            string syote;
+
+            bool jatkuu = true;
+
+            while (jatkuu)
+            {
+                Console.WriteLine("\nAnna eka luku");
+                syote = Console.ReadLine();
+                bool oikein = Int32.TryParse(syote, out luku1);
+
+                Console.WriteLine("Anna toka luku");
+                syote = Console.ReadLine();
+                oikein = Int32.TryParse(syote, out luku2);
+
+                Console.WriteLine("Anna kolmas luku");
+                syote = Console.ReadLine();
+                oikein = Int32.TryParse(syote, out luku3);
+                
+                if (oikein)
+                    {
+                        jatkuu = false;
+                        pieninLuku = Pienin(luku1, luku2, luku3);
+                        suurinLuku = Suurin(luku1, luku2, luku3);
+                    }
+            
+            else
+            {
+                Console.WriteLine("\nAnna vain numeroita!");
+            }                
+            }
+        }
+    }
+}
---
+++ Ohjelmoinnin perusteet ja Version hallinta/15_try-catch-finally/Tehtävä 15/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Tehtävä_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool calculation = true;
+
+            while (calculation)
+            {
+                try
+                {
+                    Console.WriteLine("\nAnna jaettava: ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("\nAnna jakaja: ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    
+                    calculation = false;                                        
+                    Console.WriteLine("\nJakolaskun tulos: " + num1 / num2);
+                    
+                }
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }   
+        }
+    }
+}
